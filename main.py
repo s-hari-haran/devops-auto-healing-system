0251bbcf2648a215abde84f4ffb3a921e14b709b
s-hari-haran <100272175+s-hari-haran@users.noreply.github.com>
2025-10-05 20:08:15 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -182,4 +182,6 @@ def webhook():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(host='0.0.0.0', port=port, debug=debug)
+    
